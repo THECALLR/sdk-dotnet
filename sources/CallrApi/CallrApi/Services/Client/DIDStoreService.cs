5fed52e8339731e53553347f7b2e31a04e033c30
François VIARD <francois.viard@callr.com>
2017-07-13 11:08:53 +0200

    Ajout des methodes demandées :
    did/store.cancel_order
    did/store.cancel_subscription
    did/store.get_quota_status
    did/store.get_quote
    did/store.view_order

--- sources/CallrApi/CallrApi/Services/Client/DIDStoreService.cs
+++ sources/CallrApi/CallrApi/Services/Client/DIDStoreService.cs
@@ -31,11 +31,14 @@ namespace CallrApi.Services.Client
         /// <summary>
         ///  This method reserve DID
         /// </summary>
-        /// <param name="prefixToOrder"></param>
+        /// <param name="AreaCodeID"></param>
+        /// <param name="DidClassType"></param>
+        /// <param name="Quantity"></param>
+        /// <param name="Sequence"></param>
         /// <returns></returns>
-        public string ReserveDid(int prefixToOrder)
+        public string ReserveDid(int AreaCodeID,string DidClassType, int Quantity, string Sequence)
         {
-            List<object> parameters = new List<object>() { prefixToOrder, "CLASSIC", 1, "RANDOM" };
+            List<object> parameters = new List<object>() { AreaCodeID, DidClassType, Quantity, Sequence };
             dynamic response = this.client.MakeRequest("did/store.reserve", parameters);
             return response.result["token"].ToString();
         }
@@ -49,6 +52,62 @@ namespace CallrApi.Services.Client
             List<object> parameters = new List<object>() { token };
             dynamic response = this.client.MakeRequest("did/store.buy_order", parameters);
         }
+
+        /// <summary>
+        ///  This method allow to cancel an order
+        /// </summary>
+        /// <param name="token"></param>
+        public void Cancel(string token)
+        {
+            List<object> parameters = new List<object>() { token };
+            dynamic response = this.client.MakeRequest("did/store.cancel_order", parameters);
+        }
+
+        /// <summary>
+        /// This method allow to cancel a DID subscription
+        /// </summary>
+        /// <param name="hash"></param>
+        public void CancelSubscription(string hash){
+            List<object> parameters = new List<object>() { hash };
+            dynamic response = this.client.MakeRequest("did/store.cancel_subscription", parameters);
+        }
+
+        /// <summary>
+        /// This method allow to get quota status
+        /// </summary>
+        /// <param></param>
+        /// <returns></returns>
+        public List<object> GetQuotaStatus(){
+            List<object> parameters = new List<object>() {};
+            dynamic response = this.client.MakeRequest("did/store.get_quota_status", parameters);
+            return reponse.result;
+        }
+
+        /// <summary>
+        /// This method allow to get a quote without reserving DIDs
+        /// </summary>
+        /// <param name="AreaCodeID"></param>
+        /// <param name="DidClassType"></param>
+        /// <param name="Quantity"></param>
+        /// <returns></returns>
+        public List<object> GetQuote(int AreaCodeID,string DidClassType, int Quantity){
+            List<object> parameters = new List<object>() {AreaCodeID, DidClassType, Quantity};
+            dynamic response = this.client.MakeRequest("did/store.get_quote", parameters);
+            return reponse.result;
+        }
+
+
+        ///<summary>
+        /// This method allow to view your order 
+        ///</summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public List<object> ViewOrder(string token){
+            List<object> parameters = new List<object>() { token };
+            dynamic response = this.client.MakeRequest("did/store.view_order", parameters);
+            return reponse.result;
+        }
+
         #endregion
     }
 }
